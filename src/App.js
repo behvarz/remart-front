afeef0006cc60176c629f9581c9d7ababd505117
behvarz <76514063+behvarz@users.noreply.github.com>
2025-10-25 23:26:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+console.log("✅ API URL:", process.env.REACT_APP_API_URL);
+
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
